f3e326ebc208b2625d3b8bfc5a438d2a1fb4e85f
김석진 <sj01048260279@gmail.com>
2023-12-07 15:33:15 +0900

    chore: font 세팅

---
+++ src/styles/font.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export const FontCSS = css`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/fonts/Pretendard-Regular.woff2') format('woff2');
+  }
+`;
