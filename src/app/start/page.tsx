0207d97a47ec34eccafa33347895c57dd5e7391a
김석진 <sj01048260279@gmail.com>
2023-12-10 21:41:48 +0900

    feat: start page 퍼블리싱

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import Image from 'next/image';
+import { Button } from '@/components/common';
+import styled from '@emotion/styled';
+
 const StartPage = () => {
-  return <div>sa</div>;
+  return (
+    <StyledStartPage>
+      <Image
+        src="/png/start-logo.png"
+        width={260}
+        height={151}
+        style={{ marginTop: '233px' }}
+        alt="Start Logo"
+      />
+      <Button>시작하기</Button>
+    </StyledStartPage>
+  );
 };
 
 export default StartPage;
+
+const StyledStartPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 24px;
+  min-height: 100vh;
+  width: 100%;
+`;
