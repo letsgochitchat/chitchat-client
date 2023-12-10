d384df0a2d41ea06f1472510214f06c299c26e04
김석진 <sj01048260279@gmail.com>
2023-12-10 14:57:35 +0900

    feat: secondary button stroybook 작성

---
+++ src/components/common/Button/GhostButton.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { GhostButton as GhostButtonComponent } from './GhostButton';
+
+type GhostButton = typeof GhostButtonComponent;
+
+const meta: Meta<GhostButton> = {
+  argTypes: {
+    children: {
+      control: { type: 'text' },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  component: GhostButtonComponent,
+  title: 'Components/Common/GhostButton',
+};
+
+export default meta;
+
+export const Default: StoryObj<GhostButton> = {
+  args: {
+    children: 'label',
+    width: '358px',
+  },
+  render: args => <GhostButtonComponent {...args} />,
+};
