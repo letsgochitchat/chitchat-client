ced19609f1340a0292d099f919ddc1793d1acfea
김석진 <sj01048260279@gmail.com>
2023-12-10 14:21:34 +0900

    feat: theme type 추가

--- src/styles/themes/theme.ts
+++ src/styles/themes/theme.ts
@@ -1,7 +1,9 @@
+import { type Theme } from '@emotion/react';
+
 import { colors } from './colors';
 import { fonts } from './fonts';
 
-export const theme = {
+export const theme: Theme = {
   colors,
   fonts,
 };
