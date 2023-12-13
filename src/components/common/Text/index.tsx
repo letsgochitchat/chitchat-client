c9ebd0ed7037bb9d8ab8f7e8d2d5470c5ac3d8b9
김석진 <sj01048260279@gmail.com>
2023-12-13 09:48:12 +0900

    feat: text 디자인 시스템 개발

---
+++ src/components/common/Text/index.tsx
@@ -0,0 +1,26 @@
+import { type ForwardedRef, forwardRef, type HTMLAttributes, type ReactNode } from 'react';
+import { type ColorKeys, type FontKeys } from '@/styles';
+import styled from '@emotion/styled';
+
+export type TextProps = {
+  styleType: FontKeys;
+  tag?: 'p' | 'span';
+  color?: ColorKeys;
+  children: ReactNode;
+} & HTMLAttributes<HTMLSpanElement>;
+
+export const Text = forwardRef(function Text(
+  { tag = 'span', children, styleType: textStyle = 'p1', color = 'gray900', ...props }: TextProps,
+  ref: ForwardedRef<HTMLSpanElement>
+) {
+  return (
+    <StyledText ref={ref} as={tag} color={color} styleType={textStyle} {...props}>
+      {children}
+    </StyledText>
+  );
+});
+
+const StyledText = styled.span<TextProps>`
+  color: ${({ color, theme }) => color && theme.colors[color]};
+  ${({ theme, styleType }) => theme.fonts[styleType]};
+`;
