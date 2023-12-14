583f8adc25d1e2894162f5b19e82bc6f2454e7be
김석진 <sj01048260279@gmail.com>
2023-12-14 17:33:08 +0900

    feat: header 디자인 시스템 개발

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,47 @@
+import { type HTMLAttributes } from 'react';
+import styled from '@emotion/styled';
+
+import { ArrowBackIcon, CloseIcon } from '../Icons';
+import { Text } from '..';
+
+type HeaderProps = {
+  title: string;
+  onClose?: VoidFunction;
+  onBack?: VoidFunction;
+} & HTMLAttributes<HTMLDivElement>;
+
+export const Header = ({ title, onClose, onBack, ...props }: HeaderProps) => {
+  return (
+    <StyledHeader {...props}>
+      {onBack ? (
+        <ArrowBackIcon onClick={onBack} width={32} height={32} cursor="pointer" />
+      ) : (
+        <StyledGap />
+      )}
+      <Text color="white" styleType="h4">
+        {title}
+      </Text>
+      {onClose ? (
+        <CloseIcon onClick={onClose} width={32} height={32} cursor="pointer" />
+      ) : (
+        <StyledGap />
+      )}
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 480px;
+  width: 100%;
+  height: 48px;
+  padding: 8px 16px;
+  background-color: ${({ theme }) => theme.colors.gray900};
+`;
+
+const StyledGap = styled.span`
+  width: 32px;
+  height: 32px;
+`;
