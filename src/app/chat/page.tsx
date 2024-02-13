142320b93f985916c3539e60bef841a473a42b13
이명재 <arkk20000@gmail.com>
2024-02-13 19:58:46 +0900

    feat: swiper 컴포넌트로 채팅방 스와이프 개발

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,31 @@
 'use client';
 
+import { MainHeader } from '@/components/common';
 import styled from '@emotion/styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
 
 const ChatPage = () => {
-  return <StyledChatPage>chat page</StyledChatPage>;
+  return (
+    <StyledChatPage>
+      <MainHeader />
+
+      <StyledSwiper>
+        <SwiperSlide>메인 페이지</SwiperSlide>
+        <SwiperSlide>
+          <StyledSwiper
+            onSlideNextTransitionEnd={swiper => {
+              swiper.slideTo(0, 0, false);
+            }}
+          >
+            <SwiperSlide>채팅 페이지</SwiperSlide>
+            <SwiperSlide>애니메이션용 페이지</SwiperSlide>
+          </StyledSwiper>
+        </SwiperSlide>
+      </StyledSwiper>
+    </StyledChatPage>
+  );
 };
 
 export default ChatPage;
@@ -12,8 +34,11 @@ const StyledChatPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  padding-bottom: 24px;
-  min-height: 100vh;
+  height: 100vh;
+  width: 100%;
+`;
+
+const StyledSwiper = styled(Swiper)`
+  height: 100%;
   width: 100%;
 `;
